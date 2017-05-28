7b478f2f47e7e6cbd5de3fd4cc241db8b14e4f1e
Steven Shaw <steven@steshaw.org>
2017-05-28 17:21:07 +1000

    p2p example

---
+++ 02-topologies/03-peer-to-peer/Main.hs
@@ -0,0 +1,32 @@
+#!/usr/bin/env stack
+{-
+  stack script --resolver lts-8.5
+    --package distributed-process
+    --package distributed-process-p2p
+    --
+    -Wall -fwarn-tabs
+-}
+
+import System.Environment (getArgs)
+import Control.Concurrent (threadDelay)
+import Control.Distributed.Process
+import Control.Distributed.Process.Node (initRemoteTable)
+import Control.Distributed.Backend.P2P as P2P
+import Control.Monad (forever, mapM_)
+
+helloProcess = do
+  liftIO $ threadDelay 1000000 -- give dispatcher a second to discover other nodes
+  nsendPeers "echo-server" "hello!"
+
+main = do
+  [host, port] <- getArgs
+
+  P2P.bootstrap "localhost" "3001" [P2P.makeNodeId "seedhost:9000"] initRemoteTable $
+    helloProcess
+
+{-
+  backend <- initializeBackend host port initRemoteTable
+  node    <- newLocalNode backend
+  runProcess node $ forever $ do
+    getPeers >>= mapM_ $ \peer -> nsend peer "echo-server" "hello!"
+-}
