061bf2cb608a2c5728dcaa476c37d1a1c427d774
Steven Shaw <steven@steshaw.org>
2017-06-05 03:51:45 +1000

    Ping pong

---
+++ remote/ping-pong.hs
@@ -0,0 +1,52 @@
+#!/usr/bin/env stack
+{- 
+  stack script --resolver lts-8.5
+    --package binary
+    --package distributed-process
+    --package network-transport-tcp
+    --
+    -Wall -fwarn-tabs
+    -threaded -rtsopts -with-rtsopts=-N
+-}
+
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Network.Transport.TCP (createTransport, defaultTCPParameters)
+import Control.Distributed.Process
+import Control.Distributed.Process.Node
+import Data.Binary
+import GHC.Generics
+
+newtype Ping = Ping ProcessId
+  deriving Generic
+instance Binary Ping
+
+newtype Pong = Pong ProcessId
+  deriving Generic
+instance Binary Pong
+
+self :: Process ProcessId
+self = getSelfPid
+
+ping :: Process ()
+ping = do
+  Ping pid <- expect
+  self' <- self
+  send pid (Pong self')
+  ping
+
+main :: IO ()
+main = do
+  result <- createTransport "localhost" "10501" defaultTCPParameters
+  case result of
+    Left err -> print err
+    Right t -> do
+      node <- newLocalNode t initRemoteTable
+      runProcess node $ do
+        pingPid <- spawnLocal ping
+        liftIO $ print ("pingPid", pingPid)
+        self' <- self
+        send pingPid (Ping self')
+        Pong pid <- expect
+        liftIO $ print ("got pong from ", pid)
